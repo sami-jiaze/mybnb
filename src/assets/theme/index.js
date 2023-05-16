52a64cb5474f20e094a33e64badb99e196a47f37
jiaze <1244153559@qq.com>
2023-05-16 15:16:40 +0800

    feature(theme): 主题初始化

---
+++ src/assets/theme/index.js
@@ -0,0 +1,8 @@
+const ThemeA = {
+  color: {
+    primaryColor: "#ff385c",
+    secondColor: "#00848A"
+  }
+}
+
+export default ThemeA
--- src/index.js
+++ src/index.js
@@ -3,26 +3,29 @@
 // npm i react-router-dom
 // npm i normalize.css
 // npm i -D styled-components@5.3.10
-// npm i axios  
-
+// npm i axios
 
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
 import App from './App'
 import 'normalize.css'
-import '@/assets/style/reset.scss'
+import '@/assets/style/index.scss'
 import { Provider } from 'react-redux'
 import store from './store'
+import ThemeA from './assets/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Suspense fallback="loading">
     <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <ThemeProvider theme={ThemeA}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </ThemeProvider>
     </Provider>
   </Suspense>
 )
