0c9327b7db930f3bfc8d7ade1b06f5d8df3f5b7e
jiaze <1244153559@qq.com>
2023-05-17 16:24:11 +0800

    feature(components): 封装sectiontab组件

---
+++ src/components/SectionTabs/index.jsx
@@ -0,0 +1,39 @@
+// import ScrollView from '@/base-ui/scroll-view'
+import PropTypes from 'prop-types'
+import React, { memo, useState } from 'react'
+import { TabsWrapper } from './style'
+import classNames from 'classnames'
+
+const SectionTabs = memo(props => {
+  const { tabNames = [], tabClick } = props
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  function itemClickHandle(index, item) {
+    setCurrentIndex(index)
+    tabClick(index, item)
+  }
+
+  return (
+    <TabsWrapper>
+      {/* <ScrollView> */}
+      {tabNames.map((item, index) => {
+        return (
+          <div
+            key={index}
+            onClick={e => itemClickHandle(index, item)}
+            className={classNames('item', { active: index === currentIndex })}
+          >
+            {item}
+          </div>
+        )
+      })}
+      {/* </ScrollView> */}
+    </TabsWrapper>
+  )
+})
+
+SectionTabs.propTypes = {
+  tabNames: PropTypes.array
+}
+
+export default SectionTabs
