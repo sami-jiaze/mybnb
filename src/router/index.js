5b72219c437e27ea6722f80ec9e2682ad1d8149f
jiaze <1244153559@qq.com>
2023-05-22 17:16:25 +0800

    style(router): 代码格式修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,26 @@
-import React from "react"
-import { Navigate } from "react-router-dom"
+import React from 'react'
+import { Navigate } from 'react-router-dom'
 
-const Home = React.lazy(()=>import("@/views/home"))
-const Detail = React.lazy(()=>import("@/views/detail/"))
-const Overview = React.lazy(()=>import("@/views/overview/"))
-
+const Home = React.lazy(() => import('@/views/home'))
+const Detail = React.lazy(() => import('@/views/detail/'))
+const Overview = React.lazy(() => import('@/views/overview/'))
 
 const routes = [
   {
     path: '/',
-    element : <Navigate to="/home" />
+    element: <Navigate to="/home" />
   },
   {
     path: '/home',
-    element : <Home></Home>
+    element: <Home></Home>
   },
   {
     path: '/detail',
-    element : <Detail></Detail>
+    element: <Detail></Detail>
   },
   {
     path: '/overview',
-    element : <Overview></Overview>
+    element: <Overview></Overview>
   }
 ]
 
